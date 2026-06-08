639500d0a39da610b00becdb1e5c96e157024695
MjGrace-ops <mjzid81@gmail.com>
2026-06-08 19:53:14 +0000

    first commit

---
+++ src/bookstoread/BookShelf.java
@@ -0,0 +1,9 @@
+package bookstoread;
+import java.util.Collections;
+import java.util.List;
+
+public class BookShelf {
+    public list<String> books(){
+        return Collections.emptyList();
+    }
+}
