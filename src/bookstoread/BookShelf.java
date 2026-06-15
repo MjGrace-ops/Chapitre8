4098b3b3f793c4fec15a79052f5f09506921aafe
MjGrace-ops <mjzid81@gmail.com>
2026-06-15 16:12:18 +0000

    Refactorisation du model des livres

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,22 +1,25 @@
 package bookstoread;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
 
 
-    public List<String> books() {
+    public List<Book> books() {
         return Collections.unmodifiableList(books);
     }
-    public void add(String...bookToAdd) {
 
-        books.addAll(Arrays.asList(bookToAdd));
+
+    public void add(Book...bookToAdd) {
+        books.addAll(List.of(bookToAdd));
     }
 
-    public List<String> arrange() {
+    public List<Book> arrange() {
         books.stream().sorted().collect(Collectors.toList());
         return books;
     }
